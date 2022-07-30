627215a508c95bb04e243bb18448091d51e2d6f8
Germanchoo <Germanchoo@DESKTOP-IJBDORI>
2022-07-30 14:01:46 -0300

    Login UI

--- src/main/java/com/tix/login/LoginUI.java
+++ src/main/java/com/tix/login/LoginUI.java
@@ -26,57 +26,187 @@ public class LoginUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        background = new javax.swing.JPanel();
+        title_lbl = new javax.swing.JLabel();
+        icon_image = new javax.swing.JLabel();
+        icon_name = new javax.swing.JLabel();
+        logo_name = new javax.swing.JLabel();
+        logo_image = new javax.swing.JLabel();
+        city_image = new javax.swing.JLabel();
+        header = new javax.swing.JPanel();
+        exit_btn = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        user_lbl = new javax.swing.JLabel();
+        user_txt = new javax.swing.JTextField();
+        user_separator = new javax.swing.JSeparator();
+        password_lbl = new javax.swing.JLabel();
+        password_separator = new javax.swing.JSeparator();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        login_btn = new javax.swing.JPanel();
+        login_txt = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocationByPlatform(true);
+        setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(800, 500));
+        setResizable(false);
+
+        background.setBackground(new java.awt.Color(255, 255, 255));
+        background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        title_lbl.setFont(new java.awt.Font("Roboto Black", 1, 24)); // NOI18N
+        title_lbl.setText("INICIAR SESIÓN");
+        background.add(title_lbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, 190, 30));
+
+        icon_image.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        icon_image.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/tix/images/favicon.png"))); // NOI18N
+        background.add(icon_image, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 50, 40, 30));
+
+        icon_name.setFont(new java.awt.Font("Roboto Black", 1, 24)); // NOI18N
+        icon_name.setText("LOGO");
+        background.add(icon_name, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 50, 70, 30));
+
+        logo_name.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        logo_name.setForeground(new java.awt.Color(255, 255, 255));
+        logo_name.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        logo_name.setText("NOMBRE DE LA EMPRESA");
+        background.add(logo_name, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 240, 290, 40));
+
+        logo_image.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        logo_image.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/tix/images/logo.png"))); // NOI18N
+        background.add(logo_image, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 90, 290, 150));
+
+        city_image.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        city_image.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/tix/images/city.png"))); // NOI18N
+        background.add(city_image, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 0, 290, 500));
+
+        header.setBackground(new java.awt.Color(255, 255, 255));
+
+        exit_btn.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("Roboto Light", 0, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("X");
+        jLabel1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        javax.swing.GroupLayout exit_btnLayout = new javax.swing.GroupLayout(exit_btn);
+        exit_btn.setLayout(exit_btnLayout);
+        exit_btnLayout.setHorizontalGroup(
+            exit_btnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
+        );
+        exit_btnLayout.setVerticalGroup(
+            exit_btnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout headerLayout = new javax.swing.GroupLayout(header);
+        header.setLayout(headerLayout);
+        headerLayout.setHorizontalGroup(
+            headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(headerLayout.createSequentialGroup()
+                .addComponent(exit_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 760, Short.MAX_VALUE))
+        );
+        headerLayout.setVerticalGroup(
+            headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(exit_btn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        background.add(header, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 40));
+
+        user_lbl.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
+        user_lbl.setText("USUARIO");
+        background.add(user_lbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 70, -1));
+
+        user_txt.setFont(new java.awt.Font("Roboto", 0, 12)); // NOI18N
+        user_txt.setForeground(new java.awt.Color(204, 204, 204));
+        user_txt.setText("Ingrese su nombre de usuario");
+        user_txt.setBorder(null);
+        user_txt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                user_txtActionPerformed(evt);
+            }
+        });
+        background.add(user_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 230, 390, -1));
+
+        user_separator.setForeground(new java.awt.Color(0, 0, 0));
+        background.add(user_separator, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, 390, 10));
+
+        password_lbl.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
+        password_lbl.setText("CONTRASEÑA");
+        background.add(password_lbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 280, 100, -1));
+
+        password_separator.setForeground(new java.awt.Color(0, 0, 0));
+        background.add(password_separator, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 330, 390, 10));
+
+        jPasswordField1.setForeground(new java.awt.Color(204, 204, 204));
+        jPasswordField1.setText("jPasswordField1");
+        jPasswordField1.setBorder(null);
+        background.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, 390, -1));
+
+        login_btn.setBackground(new java.awt.Color(0, 134, 190));
+
+        login_txt.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        login_txt.setForeground(new java.awt.Color(255, 255, 255));
+        login_txt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        login_txt.setText("ENTRAR");
+        login_txt.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        javax.swing.GroupLayout login_btnLayout = new javax.swing.GroupLayout(login_btn);
+        login_btn.setLayout(login_btnLayout);
+        login_btnLayout.setHorizontalGroup(
+            login_btnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, login_btnLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(login_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        login_btnLayout.setVerticalGroup(
+            login_btnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, login_btnLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(login_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        background.add(login_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 380, 140, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void user_txtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_user_txtActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_user_txtActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoginUI().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel background;
+    private javax.swing.JLabel city_image;
+    private javax.swing.JPanel exit_btn;
+    private javax.swing.JPanel header;
+    private javax.swing.JLabel icon_image;
+    private javax.swing.JLabel icon_name;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JPanel login_btn;
+    private javax.swing.JLabel login_txt;
+    private javax.swing.JLabel logo_image;
+    private javax.swing.JLabel logo_name;
+    private javax.swing.JLabel password_lbl;
+    private javax.swing.JSeparator password_separator;
+    private javax.swing.JLabel title_lbl;
+    private javax.swing.JLabel user_lbl;
+    private javax.swing.JSeparator user_separator;
+    private javax.swing.JTextField user_txt;
     // End of variables declaration//GEN-END:variables
 }
